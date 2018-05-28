d6ed9054c0a387b0567236d7188b4ea183771a3d
wanbo.ge <wanbo.ge@condenast.com.cn>
2018-05-28 17:58:02 +0800

    Added files

---
+++ web_root/demo_app/index.php
@@ -0,0 +1,13 @@
+<?php
+/**************************************************
+ * Copyright (c).
+ * Filename: index.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/5/28
+ * Description:
+ **************************************************/
+
+$app = XL_Init::init();
+
+$app->bootstrap()
+    ->run();
