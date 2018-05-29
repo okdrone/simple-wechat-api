2ced799d7588c1fd416197170bfb016a4955bda2
wanbo.ge <wanbo.ge@condenast.com.cn>
2018-05-29 16:13:26 +0800

    update

--- library/app/app_core.php
+++ library/app/app_core.php
@@ -21,7 +21,10 @@ class APP_Core
         echo 'root_path:';
         var_dump(ROOT_PATH);
 
-        new Yaf_Application(ROOT_PATH . "/conf/app/demo_app/app.ini");
+        $objYCI = new Yaf_Config_Ini(ROOT_PATH . '/conf/app/demo_app/app.ini');
+        $yaf_conf = $objYCI->toArray();
+        $yaf_conf['application']['directory'] = ROOT_PATH . '/app/demo_app';
+        new Yaf_Application($yaf_conf);
 
         self::$isInit = true;
 
