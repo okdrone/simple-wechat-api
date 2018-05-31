2b99296323a4d7ce6b38207dbda891a6147a8681
gewanbo@gmail.com <gewanbo@gmail.com>
2018-05-31 11:17:35 +0800

    Update

--- app/wechat/controllers/Index.php
+++ app/wechat/controllers/Index.php
@@ -9,13 +9,23 @@
  **************************************************/
 class Controller_Index extends Yaf_Controller_Abstract
 {
+    protected $logger;
 
     public function init() {
         Yaf_Dispatcher::getInstance()->autoRender(FALSE);
+
+        $this->logger = new AppLogger();
     }
 
     public function greetingAction(){
         echo "Greeting:";
         echo 'Hello world!';
+    }
+
+    public function messageServiceAction(){
+
+        $this->logger->info('This is a info log.');
+
+        echo 'This is message service';
     }
 }
