1e84c21ed1af9af82788d7ecacc299ed30c315b3
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-02 07:46:39 +0000

    Setting which section to read

--- library/App/Core.php
+++ library/App/Core.php
@@ -35,10 +35,11 @@ class APP_Core
     static public function initYaf() {
 
         $objYCI = new Yaf_Config_Ini(CONF_PATH . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . self::appName() .
-            DIRECTORY_SEPARATOR . 'app.ini');
+            DIRECTORY_SEPARATOR . 'app.ini', 'application');
         $yaf_conf = $objYCI->toArray();
         $yaf_conf['application']['directory'] = APP_PATH . DIRECTORY_SEPARATOR . self::appName();
         $yaf_conf['application']['library']['directory'] = $yaf_conf['application']['directory'] . DIRECTORY_SEPARATOR . 'library';
+
         new Yaf_Application($yaf_conf);
 
     }
