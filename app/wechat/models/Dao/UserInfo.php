1307e0a24599219bc840fc3077a480e624f99b22
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-04 16:10:48 +0800

    UserInfo Dao.

---
+++ app/wechat/models/Dao/UserInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+/**************************************************
+ * Copyright (c).
+ * Filename: UserInfo.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/4
+ * Description:
+ **************************************************/
+class Dao_UserInfo
+{
+    public $user_id = null;
+    public $username = null;
+    public $nickname = null;
+    public $icon = null;
+    public $phone = null;
+    public $create_ts = null;
+}
