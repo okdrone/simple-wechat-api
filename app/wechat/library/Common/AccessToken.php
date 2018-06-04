e9c93009c2d542a2b8a7c44899da621521e9b559
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-04 19:10:49 +0800

    AccessToken common features.

---
+++ app/wechat/library/Common/AccessToken.php
@@ -0,0 +1,86 @@
+<?php
+/**************************************************
+ * Copyright (c).
+ * Filename: AccessToken.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/4
+ * Description:
+ **************************************************/
+
+namespace Common;
+
+
+class AccessToken
+{
+    private static $accessToken = [];
+
+    public static function getAccessToken($conf){
+
+        $appId = $conf['appid'];
+
+        /**
+         * 1. Get from local
+         */
+
+        if(isset(self::$accessToken[$appId])){
+            $tokenData = self::$accessToken[$appId];
+            if(self::isValidToken($tokenData)){
+                return $tokenData['access_token'];
+            }
+        }
+
+        /**
+         * 2. Get from database
+         */
+        $serviceAccessToken = new \Service_Wechat_AccessToken();
+
+        $accessTokenObj = $serviceAccessToken->getAccessTokenByApp($appId);
+        if($accessTokenObj instanceof \Dao_UserInfo && !empty($accessTokenObj->access_token)) {
+            $tokenData = array(
+                'access_token' => $accessTokenObj->access_token,
+                'create_ts' => $accessTokenObj->create_ts,
+                'expire_ts' => $accessTokenObj->expire_ts
+            );
+
+            if(self::isValidToken($tokenData)){
+                return $tokenData['access_token'];
+            }
+        }
+
+        /**
+         * 3. Get from Wechat API and sync to database
+         */
+
+        $current_time = time();
+        $url = sprintf('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s', $appId, $conf['appsecret']);
+        $res = Curl::get($url);
+        if (!empty($res)) {
+            $ret = json_decode($res, true);
+            $strAccessToken = $ret['access_token'];
+            self::$accessToken[$appId] = array(
+                'access_token' => $strAccessToken,
+                'create_ts' => $current_time,
+                'expire_ts' => $ret['expires_in'],
+                'app_id' => $appId,
+                'type' => 1,
+            );
+
+            $objAccessToken = new \Dao_AccessToken();
+            $objAccessToken->access_token = $strAccessToken;
+            $objAccessToken->create_ts = $current_time;
+            $objAccessToken->expire_ts = $ret['expires_in'];
+            $objAccessToken->type = 1;
+            $objAccessToken->app_id = $appId;
+
+            $serviceAccessToken->setAccessTokenByApp($objAccessToken);
+
+            return $strAccessToken;
+        } else {
+            return '';
+        }
+    }
+
+    private static function isValidToken(array $tokenData){
+        return $tokenData['create_ts'] + $tokenData['expire_ts'] > time() + 100;
+    }
+}
