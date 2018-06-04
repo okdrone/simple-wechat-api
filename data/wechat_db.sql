2a5ddde21f54a91c12b4352b6d11467b9041fd69
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-04 16:43:57 +0800

    Added new tables;

--- data/wechat_db.sql
+++ data/wechat_db.sql
@@ -11,3 +11,29 @@ CREATE TABLE `xyz_user_info` (
   KEY `status` (`status`),
   KEY `create_ts` (`create_ts`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='User Information';
+
+DROP TABLE IF EXISTS `xyz_user_open_info`;
+CREATE TABLE `xyz_user_open_info` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Auto increase id',
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'UserId related from user_info',
+  `open_type` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '1: Wechat',
+  `open_app_id` varchar(200) NOT NULL DEFAULT '' COMMENT 'Open appid',
+  `open_user_id` varchar(200) NOT NULL DEFAULT '' COMMENT 'user_id or openid in Open platform',
+  `create_ts` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Create timestamp',
+  PRIMARY KEY (`id`),
+  KEY `open_user_id` (`open_user_id`),
+  KEY `create_ts` (`create_ts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='User of open platform';
+
+
+DROP TABLE IF EXISTS `xyz_wechat_access_token`;
+
+CREATE TABLE `xyz_wechat_access_token` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Auto increase id',
+  `app_id` varchar(128) NOT NULL DEFAULT '' COMMENT 'Wechat appid',
+  `access_token` varchar(256) NOT NULL DEFAULT '' COMMENT 'access_token from wechat api',
+  `create_ts` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Create time seconds',
+  `expire_ts` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Expire time seconds',
+  `type` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT 'TYPE. 1=ACCESS_TOKEN, 2=JSAPI_TICKET, ...',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Wechat access_token maintenance table.';
