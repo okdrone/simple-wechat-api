79d174d60aeadd852e5158fb0969ddb2474acc66
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-04 18:34:30 +0800

    Common curl class.

---
+++ app/wechat/library/Common/Curl.php
@@ -0,0 +1,47 @@
+<?php
+/**************************************************
+ * Copyright (c).
+ * Filename: Curl.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/4
+ * Description:
+ **************************************************/
+
+namespace Common;
+
+
+class Curl
+{
+    private static $default_opts = array(
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_TIMEOUT => 10
+    );
+
+    private static function send($opts)
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    public static function get($url){
+        $arr_opts = array_merge(self::$default_opts, array(
+            CURLOPT_URL => $url
+        ));
+
+        return self::send($arr_opts);
+    }
+
+    public static function post($url, $data){
+        $arr_opts = array_merge(self::$default_opts, array(
+            CURLOPT_URL => $url,
+            CURLOPT_POSTFIELDS => $data
+        ));
+
+        return self::send($arr_opts);
+    }
+}
