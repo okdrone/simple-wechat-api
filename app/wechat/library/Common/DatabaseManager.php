abde1b0afc68ea1f075e37223a4c475798c22b56
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-04 14:53:34 +0800

    Database config.

---
+++ app/wechat/library/Common/DatabaseManager.php
@@ -0,0 +1,29 @@
+<?php
+/**************************************************
+ * Copyright (c).
+ * Filename: DatabaseManager.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/4
+ * Description:
+ **************************************************/
+
+namespace Common;
+
+
+class DatabaseManager
+{
+
+    static protected $databaseList = [];
+
+    public static function getInstance($db_name){
+        if(!key_exists($db_name, self::$databaseList)){
+
+            $conf = \App_Config::getDBConfig('wechat', 'application');
+
+            var_dump($conf);
+
+        }
+
+        return self::$databaseList[$db_name];
+    }
+}
