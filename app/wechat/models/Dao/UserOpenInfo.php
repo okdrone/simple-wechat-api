16c33bd851b23f2fe300f00a888d74218c7dca9b
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-05 18:22:29 +0800

    Added new model.

---
+++ app/wechat/models/Dao/UserOpenInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+/**************************************************
+ * Copyright (c).
+ * Filename: UserOpenInfo.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/5
+ * Description:
+ **************************************************/
+class Dao_UserOpenInfo
+{
+    public $id = null;
+    public $user_id = null;
+    public $open_type = null;
+    public $open_app_id = null;
+    public $open_user_id = null;
+    public $create_ts = null;
+}
