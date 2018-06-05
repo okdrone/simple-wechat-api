cb5b9221899c5ade31e28def1d9d654cab11b7d1
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-05 14:54:30 +0800

    Update

--- app/wechat/models/Service/Wechat/AccessToken.php
+++ app/wechat/models/Service/Wechat/AccessToken.php
@@ -18,7 +18,7 @@ class Service_Wechat_AccessToken
 
         if($db instanceof PDO){
             $stm = $db->query('SELECT * FROM xyz_wechat_access_token where appid=:appid and `type`=1');
-            $stm->bindValue(':username', $appid, PDO::PARAM_STR);
+            $stm->bindValue(':appid', $appid, PDO::PARAM_STR);
             $stm->execute();
             $ret = $stm->fetch(PDO::FETCH_ASSOC);
 
