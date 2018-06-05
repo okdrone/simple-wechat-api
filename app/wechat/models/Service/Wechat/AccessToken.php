ee470cdf9794634b6f5555937fe6e213839d08d0
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-05 15:33:43 +0800

    Update

--- app/wechat/models/Service/Wechat/AccessToken.php
+++ app/wechat/models/Service/Wechat/AccessToken.php
@@ -9,25 +9,38 @@
  **************************************************/
 class Service_Wechat_AccessToken
 {
+    protected $logger;
 
-    public function getAccessTokenByApp($appid){
+    public function __construct()
+    {
+        $this->logger = new Logger_App();
+    }
+
+    public function getAccessTokenByApp($app_id){
 
         $accessToken = new Dao_AccessToken();
 
-        $db = \Common\DatabaseManager::getInstance('xyz');
+        try {
 
-        if($db instanceof PDO){
-            $stm = $db->prepare('SELECT * FROM xyz_wechat_access_token where appid=:appid and `type`=1');
-            $stm->bindParam(':appid', $appid, PDO::PARAM_STR);
-            $stm->execute();
-            $ret = $stm->fetch(PDO::FETCH_ASSOC);
+            $db = \Common\DatabaseManager::getInstance('xyz');
 
-            if($ret) {
-                $accessToken->id = $ret['id'];
-                $accessToken->access_token = $ret['access_token'];
-                $accessToken->create_ts = $ret['create_ts'];
-                $accessToken->expire_ts = $ret['expire_ts'];
+            if ($db instanceof PDO) {
+                $stm = $db->prepare('SELECT * FROM xyz_wechat_access_token where app_id=:app_id and `type`=1');
+                $stm->bindParam(':app_id', $app_id, PDO::PARAM_STR);
+                $stm->execute();
+                $ret = $stm->fetch(PDO::FETCH_ASSOC);
+
+                if ($ret) {
+                    $accessToken->id = $ret['id'];
+                    $accessToken->access_token = $ret['access_token'];
+                    $accessToken->create_ts = $ret['create_ts'];
+                    $accessToken->expire_ts = $ret['expire_ts'];
+                }
+            } else {
+                new Exception('The $db is not instance of PDO when connecting to database.');
             }
+        } catch (Exception $e){
+            $this->logger->error($e->getMessage(), $e->getTrace());
         }
 
         return $accessToken;
@@ -37,16 +50,24 @@ class Service_Wechat_AccessToken
 
         $ret = false;
 
-        $db = \Common\DatabaseManager::getInstance('xyz');
+        try {
 
-        if($db instanceof PDO){
-            $stm = $db->prepare('INSERT INTO xyz_wechat_access_token (`app_id`, `type`, `access_token`, `create_ts`, `expire_ts`) VALUE (:app_id, :type, :access_token, :create_ts, :expire_ts)');
-            $stm->bindParam(':app_id', $accessToken->app_id, PDO::PARAM_STR);
-            $stm->bindParam(':type', $accessToken->type, PDO::PARAM_INT);
-            $stm->bindParam(':access_token', $accessToken->access_token, PDO::PARAM_STR);
-            $stm->bindParam(':create_ts', $accessToken->create_ts, PDO::PARAM_INT);
-            $stm->bindParam(':expire_ts', $accessToken->expire_ts, PDO::PARAM_INT);
-            $ret = $stm->execute();
+            $db = \Common\DatabaseManager::getInstance('xyz');
+
+            if ($db instanceof PDO) {
+                $stm = $db->prepare('INSERT INTO xyz_wechat_access_token (`app_id`, `type`, `access_token`, `create_ts`, `expire_ts`) VALUE (:app_id, :type, :access_token, :create_ts, :expire_ts)');
+                $stm->bindParam(':app_id', $accessToken->app_id, PDO::PARAM_STR);
+                $stm->bindParam(':type', $accessToken->type, PDO::PARAM_INT);
+                $stm->bindParam(':access_token', $accessToken->access_token, PDO::PARAM_STR);
+                $stm->bindParam(':create_ts', $accessToken->create_ts, PDO::PARAM_INT);
+                $stm->bindParam(':expire_ts', $accessToken->expire_ts, PDO::PARAM_INT);
+                $ret = $stm->execute();
+            } else {
+                new Exception('The $db is not instance of PDO when connecting to database.');
+            }
+
+        } catch (Exception $e){
+            $this->logger->error($e->getMessage(), $e->getTrace());
         }
 
         return $ret;
