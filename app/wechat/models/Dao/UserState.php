78483abcf85a4c517647825d26d3d8ab9df4e313
gewanbo@gmail.com <gewanbo@gmail.com>
2018-06-06 16:03:39 +0800

    User state

---
+++ app/wechat/models/Dao/UserState.php
@@ -0,0 +1,14 @@
+<?php
+
+/**************************************************
+ * Copyright (c).
+ * Filename: UserState.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/6/6
+ * Description:
+ **************************************************/
+class Dao_UserState
+{
+    const ENABLE = 0;
+    const DISABLE = 1;
+}
