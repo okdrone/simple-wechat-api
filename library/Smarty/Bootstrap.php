be93c295b21933df94e8334e6a37b8e4ffe61c9f
gewanbo@gmail.com <gewanbo@gmail.com>
2018-07-13 14:31:01 +0800

    Added Smarty engine

---
+++ library/Smarty/Bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+/**************************************************
+ * Copyright (c).
+ * Filename: Smarty_Bootstrap.php
+ * Author: Wanbo Ge <gewanbo@gmail.com>
+ * Create Date: 2018/7/13
+ * Description:
+ **************************************************/
+
+Yaf_Loader::import( dirname(__FILE__)."/libs/Smarty.class.php");
+Yaf_Loader::import( dirname(__FILE__)."/libs/Autoloader.php");
+
+class Smarty_Bootstrap implements Yaf_View_Interface
+{
+
+    protected $smarty;
+
+    public function __construct()
+    {
+        Smarty_Autoloader::register();
+        $this->smarty = new Smarty();
+    }
+
+    public function display($tpl){
+        echo $this->smarty->fetch($tpl);
+    }
+}
